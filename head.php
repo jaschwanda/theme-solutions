4f6b89a38d5fae359d9f0a0c5115474c8270bbf1
jaschwanda <iJAS@usi2solve.com>
2021-06-13 09:51:52 -0400

    Added favicon and error_get_last() support.

--- head.php
+++ head.php
@@ -1,21 +1,24 @@
 <!doctype html>
 <?php // ------------------------------------------------------------------------------------------------------------------------ //
-/* Version: 1.5.2 (2021-03-22) */
+/* Version: 1.5.2 (2021-06-09) */
 global $usi_theme_title;
-$usi_theme_base_url = !empty(USI_Theme_Solutions::$options['header']['base_url']);
-$usi_theme_favicon  = !empty(USI_Theme_Solutions::$options['header']['favicon']);
-$usi_theme_title    = (!empty($usi_theme_title) ? $usi_theme_title : get_the_title()) . 
-   (!empty(USI_Theme_Solutions::$options['search']['page_title_suffix']) 
-    ? USI_Theme_Solutions::$options['search']['page_title_suffix'] 
-    : ''
-   );
+if (empty($usi_theme_title)) $usi_theme_title = get_the_title() . USI_Theme_Solutions::$options['search']['page_title_suffix'] ?? '';
 // --------------------------------------------------------------------------------------------------------------------------- // ?>
 <html class="no-js" lang="en">
   <head>
 <?php 
-   if ($usi_theme_base_url) echo '    <base href="' . USI_Theme_Solutions::$home_url . '" /><!--' . $_SERVER['SERVER_ADDR'] . '-->' . PHP_EOL;
-   if ($usi_theme_favicon)  echo '    <link href="' . USI_Theme_Solutions::$options['header']['favicon'] . '" rel="shortcut icon">' . PHP_EOL;
+
+   if (!empty(USI_Theme_Solutions::$options['header']['base_url'])) {
+      echo '    <base href="' . USI_Theme_Solutions::$home_url . '" /><!--' . $_SERVER['SERVER_ADDR'] . '-->' . PHP_EOL;
+   }
+
+   if (!empty(USI_Theme_Solutions::$options['header']['favicon'])) {
+      echo '    <link href="' . USI_Theme_Solutions::$options['header']['favicon'] . '" rel="shortcut icon">' . PHP_EOL;
+   }
+
    wp_head();
+
    echo '    <title>' . $usi_theme_title . '</title>' . PHP_EOL;
+
 ?>
   </head>
